9217a00590ee5058800296922770a641519cae00
Omambia Dauglous <omambiadauglous@gmail.com>
2022-01-31 15:06:25 +0300

    Fix Ts Config File

--- build/index.d.ts
+++ build/index.d.ts
@@ -3,8 +3,3 @@ export * from "./utils/fund";
 export * from "./funds/create-fund";
 export * from "./subgraph/funds";
 export * from "./aave/index";
-<<<<<<< HEAD
-=======
-export * from './funds/withdrawal-deposits';
-export * from './trading/investment-redeem';
->>>>>>> 563a4e112456d8feb3cdf9304032aa8d9cf54798
